10ee9fb422722ba828b885e0ac746b0c3b806e31
Maksym <maximka0601@gmail.com>
2024-08-08 00:28:31 +0300

    end

---
+++ src/js/form.js
@@ -0,0 +1,48 @@
+let formData = {
+    email: '',
+    message: '',
+  }
+  
+  const form = document.querySelector('.feedback-form')
+  const emailInput = form.querySelector('input')
+  const messageInput = form.querySelector('textarea')
+  
+  fillForm()
+  
+  form.addEventListener('input', handleFormInpit)
+  form.addEventListener('submit', handleFormSubmit)
+  
+  function handleFormInpit() {
+    formData.email = emailInput.value.trim()
+    formData.message = messageInput.value.trim()
+  
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData))
+  }
+  
+  function handleFormSubmit(event) {
+    event.preventDefault()
+  
+    const isEmptyField = Object.values(formData).some(
+      value => value.trim() === ''
+    )
+  
+    if (isEmptyField) {
+      alert('Fill please all fields')
+    } else {
+      console.log(formData)
+      event.currentTarget.reset()
+      localStorage.removeItem('feedback-form-state')
+      formData = { email: '', message: '' }
+    }
+  }
+  
+  function fillForm() {
+    const savedData = localStorage.getItem('feedback-form-state')
+  
+    if (savedData) {
+      formData = JSON.parse(savedData)
+  
+      emailInput.value = formData.email
+      messageInput.value = formData.message
+    }
+  }
